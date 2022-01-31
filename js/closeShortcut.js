20cf868705740d41619ec3cf402a8754aa18df07
marcin-l <marcinx@gmail.com>
2022-01-31 05:33:09 +0100

    changed shortcut

--- js/closeShortcut.js
+++ js/closeShortcut.js
@@ -1,6 +1,6 @@
 document.onkeyup = function(e) {
-	if (e.key == "W" && e.shiftKey) {
-        if(e.srcELement.localName != "input" && e.srcELement.tagName != "INPUT" && e.srcELement.type != "text")
+	if (e.key == "q" && e.ctrlKey) {
+        //if(e.srcELement.localName != "input" && e.srcELement.tagName != "INPUT" && e.srcELement.type != "text")
         doClose();
 	}
 }

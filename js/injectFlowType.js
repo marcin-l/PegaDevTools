72c1ce53da9ca7ea62ccc33feead11dc700c8557
marcin-l <marcinx@gmail.com>
2022-07-18 22:33:15 +0200

    code quality

--- js/injectFlowType.js
+++ js/injectFlowType.js
@@ -10,7 +10,13 @@ if(document.querySelector("div.gfw-breadcrumbs ul li")) {
     document.querySelector("div.gfw-breadcrumbs ul li").insertAdjacentHTML("beforeend", " <a style='font-family: OpenSans, sans-serif; font-size: small;' id='PDTShowNodeIDs'>Show node IDs</a> ");
 
     document.querySelector("a#PDTShowNodeIDs").addEventListener("click", () => {
-        document.querySelectorAll("div#processFlow_g svg g g[id].typeAssignment, div#processFlow_g svg g g[id].typeUtility, div#processFlow_g svg g g[id].typeFork, div#processFlow_g svg g g[id].typeSubflow").forEach((e)=> {let gid = e.getAttribute('id'); console.log(e.querySelector("tspan").innerText); e.querySelector("tspan").innerHTML = gid; e.querySelectorAll("tspan:nth-of-type(n+2)").forEach(e => e.remove());});
+        document.querySelectorAll("div#processFlow_g svg g g[id].typeAssignment, div#processFlow_g svg g g[id].typeUtility, div#processFlow_g svg g g[id].typeFork, div#processFlow_g svg g g[id].typeSubflow").
+            forEach((e)=> {
+                    let gid = e.getAttribute('id'); 
+                    console.log(e.querySelector("tspan").innerText); 
+                    e.querySelector("tspan").innerHTML = gid; 
+                    e.querySelectorAll("tspan:nth-of-type(n+2)").forEach(e2 => e2.remove());
+            });
     })
 }
 

123f8029deacd13bb58c9cc5bc9f05dfcf60f582
marcin-l <marcinx@gmail.com>
2022-07-18 22:02:21 +0200

    prevent errors

--- js/closeTabMiddleClick.js
+++ js/closeTabMiddleClick.js
@@ -1,36 +1,39 @@
 const containerTabList = document.querySelector("div.tStrCntr ul");
 
-const containerTabListCallback = function (mutationsList, observer) {
-	mutationsList.forEach((mutation) => {
-		mutation.addedNodes.forEach((node) => {
-			//console.log(node.nodeName);
-			if (node.nodeName == "LI") {	//rule name				
-				node.querySelectorAll("table#RULE_KEY span[data-stl='1']").forEach(function (elem) {
-					elem.addEventListener("mousedown", function (e) {
-						//console.log(e);
-						if (e && (e.which == 2 || e.button == 4)) {
-							this.parentNode.parentNode.querySelector('#close').click();
-						}
-					})
-				}
-				);			
-			}
-			if (node.nodeName == "svg") {	//icon
-				node.addEventListener("mousedown", function (e) {
-					//console.log(e);
-						if (e && (e.which == 2 || e.button == 4)) {
-							this.parentNode.parentNode.querySelector('#close').click();
-						}
+if(containerTabList) {
+	const containerTabListCallback = function (mutationsList, observer) {
+		mutationsList.forEach((mutation) => {
+			mutation.addedNodes.forEach((node) => {
+				//console.log(node.nodeName);
+				if (node.nodeName == "LI") {	//rule name				
+					node.querySelectorAll("table#RULE_KEY span[data-stl='1']").forEach(function (elem) {
+						elem.addEventListener("mousedown", function (e) {
+							//console.log(e);
+							if (e && (e.which == 2 || e.button == 4)) {
+								this.parentNode.parentNode.querySelector('#close').click();
+							}
+						})
 					}
-				);	
+					);			
+				}
+				if (node.nodeName == "svg") {	//icon
+					node.addEventListener("mousedown", function (e) {
+						//console.log(e);
+							if (e && (e.which == 2 || e.button == 4)) {
+								this.parentNode.parentNode.querySelector('#close').click();
+							}
+						}
+					);	
+				}
 			}
-		}
-		);
-	});
-};
+			);
+		});
+	};
 
-const containerTabListObserver = new MutationObserver(containerTabListCallback);
-containerTabListObserver.observe(containerTabList, { childList: true, subtree: true });
+	const containerTabListObserver = new MutationObserver(containerTabListCallback);
+	containerTabListObserver.observe(containerTabList, { childList: true, subtree: true });
 
-console.log("PDT: closeTabMiddleClick.js loaded"); 
-
+	console.log("PDT: closeTabMiddleClick.js loaded"); 
+} else {
+	console.log("PDT: closeTabMiddleClick.js tab container not found");
+}

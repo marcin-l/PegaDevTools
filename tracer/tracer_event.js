58d98c7909548daa0123de508e455c926d121d0d
marcin-l <marcinx@gmail.com>
2022-12-15 23:21:34 +0100

    Tracer feature - remove all columns

--- tracer/tracer_event.js
+++ tracer/tracer_event.js
@@ -2,18 +2,22 @@
 $("div#traceEvent-CONTAINER").on("contextmenu", "td#eventLineNumber", function (evt) { evt.preventDefault(); $(this).parent().toggleClass("PegaDevToolsTextRed"); });
 
 //FEATURE hide columns
-var removeThreadNameButton = $('<span title="Remove" style="margin-left:3px; cursor:pointer">x</span>');
+const removeThreadNameButton = $('<span title="Remove column" class="removeColumn">x</span>');
 removeThreadNameButton.click(function () { injectStyles('td.EventDataCenter#threadname { display: none}'); $('td.eventTitleBarStyle[title="Thread"]').css('display', 'none'); });
 $('td.eventTitleBarStyle[title="Thread"]').append(removeThreadNameButton);
 
-var removeIntBtn = $('<span title="Remove" style="margin-left:3px; cursor:pointer">x</span>');
+const removeIntBtn = $('<span title="Remove column" class="removeColumn">x</span>');
 removeIntBtn.click(function () { injectStyles('div#traceEvent-CONTAINER table td:nth-child(6) { display: none}'); $('td.eventTitleBarStyle[title="Int"]').css('display', 'none'); });
 $('td.eventTitleBarStyle[title="Int"]').append(removeIntBtn);
 
-var removeElapsedBtn = $('<span title="Remove" style="margin-left:3px; cursor:pointer">x</span>');
+const removeElapsedBtn = $('<span title="Remove column" class="removeColumn">x</span>');
 removeElapsedBtn.click(function () { injectStyles('div#traceEvent-CONTAINER table td:nth-child(13) { display: none}'); $('td.eventTitleBarStyle[title="Elapsed"]').css('display', 'none'); });
 $('td.eventTitleBarStyle[title="Elapsed"]').append(removeElapsedBtn);
 
-var removeRuleNoBtn = $('<span title="Remove" style="margin-left:3px; cursor:pointer">x</span>');
+const removeRuleNoBtn = $('<span title="Remove column" class="removeColumn">x</span>');
 removeRuleNoBtn.click(function () { injectStyles('div#traceEvent-CONTAINER table td:nth-child(7) { display: none}'); $('td.eventTitleBarStyle[title="Rule#"]').css('display', 'none'); });
 $('td.eventTitleBarStyle[title="Rule#"]').append(removeRuleNoBtn);
+
+const removeAllColumnsBtn = $('<div title="Remove all columns" class="removeColumn">X</div>');
+removeAllColumnsBtn.click(function () { $('span.removeColumn').click(); $(this).css('display', 'none'); });
+$('td.eventTitleBarStyle[title="Line"]').prev().append(removeAllColumnsBtn);

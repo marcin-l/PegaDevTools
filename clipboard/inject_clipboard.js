7495f7d0f9d5a37965d072fdb3698beae6558418
marcin-l <marcinx@gmail.com>
2023-09-15 00:03:36 +0200

    button to copy property names and values

--- clipboard/inject_clipboard.js
+++ clipboard/inject_clipboard.js
@@ -13,13 +13,15 @@ function handleRightPanelChange() {
 				if(status)
 					jQuery(".heading_2").append(" (<i>" + status + "</i>)");
 			}
+			
 		}
 		//handle newAssignPage
 		else if(jQuery(".heading_2").length > 0 && jQuery(".heading_2")[0].textContent == 'newAssignPage') {
-			let addedText = "";
+			let addedText = "", cls = "";
 			let napElem = jQuery('div#gridBody_right table.gridTable td.dataLabelRead.gridCell').filter(function(){return this.textContent.trim() === "pxObjClass"});
 			if(napElem.length > 0) {
-				let cls = napElem.next()[0].innerText;
+				
+				cls = napElem.next()[0].innerText;
 				if(cls) {
 					if(cls.includes("-"))
 						cls = cls.split('-')[1];
@@ -54,12 +56,76 @@ function handleRightPanelChange() {
 			let addedFilterInput = document.querySelector("p.filter-table input");
 			if(addedFilterInput)
 				addedFilterInput.focus();
-		}
+		}	
+		
+		document.querySelectorAll("div#gridBody_right table.gridTable")[1].querySelectorAll("tr.cellCont").forEach(
+			(e) => {
+				if(e.querySelector("tr a")) {
+					let eTextProp = e.querySelector("tr a").innerText;
+					let eTextValue;
+					let eValue = e.querySelector("td div.oflowDivM span");
+					if(eValue) {
+						eTextValue = eValue.innerText.replace(/\n/g, "\\n")
+						.replace(/</g, "\\<")
+						.replace(/>/g, "\\>")
+						.replace(/"/g, "&quot;")
+						.replace(/'/g, "\\'");
+					}
+					
+					//FEATURE: copy property name to clipboard
+					if(eTextProp)
+						e.querySelector("tr a").insertAdjacentHTML('beforebegin', '<a onclick="copyToClipboard(\'.' + eTextProp + '\')"><i class="icons pi pi-copy"></i></a>&nbsp;');
+					
+					//FEATURE: copy property value to clipboard
+					if(eTextValue)
+						e.querySelector("td div.oflowDivM span").insertAdjacentHTML('beforebegin', '<a onclick="copyToClipboard(\'' + eTextValue + '\')"><i class="icons pi pi-copy"></i></a>&nbsp;')
+					
+					//FEATURE: copy property name and value to clipboard
+					if(eTextProp && eTextValue) {
+						let eText = "." + eTextProp + " = &quot;" + eTextValue + "&quot;";
+						e.querySelector("td div.oflowDivM").insertAdjacentHTML('beforeend', '<div style="float:right"><a onclick="copyToClipboard(\'' + eText + '\')"><i class="icons pi pi-copy"></i></a></div>')
+					}					
+				}
+			}
+		);
     });
 }
 
 function stripeTable(table) {
 	jQuery(table).find('tr').removeClass('striped').filter(':visible:even').addClass('striped');
+};
+
+//TODO: inject from shared file?
+function copyToClipboard(textContent) {
+	// create hidden text element, if it doesn't already exist
+	let targetId = "_hiddenCopyText_";
+	let target = document.createElement("textarea");
+	target.style.position = "absolute";
+	target.style.left = "-9999px";
+	target.style.top = "0";
+	target.id = targetId;
+	document.body.appendChild(target);
+	target.textContent = textContent;
+
+	// select the content
+	let currentFocus = document.activeElement;
+	target.focus();
+	target.setSelectionRange(0, target.value.length);
+
+	// copy the selection
+	let succeed;
+	try {
+		succeed = document.execCommand("copy");
+	} catch (e) {
+		succeed = false;
+	}
+	// restore original focus
+	if (currentFocus && typeof currentFocus.focus === "function") {
+		currentFocus.focus();
+	}
+
+	target.textContent = "";
+	return succeed;
 };
 
 if(pega && pega.ui && pega.ui.Doc) {
